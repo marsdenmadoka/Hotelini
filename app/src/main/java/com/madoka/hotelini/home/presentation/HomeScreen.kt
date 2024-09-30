c8b6a51d5b597fb24b9c48ff05d46e3f890020f6
marsdenmadoka <marsmadoka98@gmail.com>
2024-10-01 01:16:26 +0300

    added launch effects see you tommorow

--- app/src/main/java/com/madoka/hotelini/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/madoka/hotelini/home/presentation/HomeScreen.kt
@@ -1,5 +1,9 @@
 package com.madoka.hotelini.home.presentation
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.content.pm.PackageManager
+import android.location.Location
+import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -16,18 +20,25 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.libraries.places.api.Places
 import com.madoka.hotelini.BuildConfig
 import com.madoka.hotelini.Manifest
@@ -59,8 +70,7 @@ fun HomeScreen(
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun HomeScreenContent(
-    state: HomeUiState,
-    onEvent: (HomeUiEvents) -> Unit,
+
 ) {
     val context = LocalContext.current
     Places.initialize(context, BuildConfig.MAPS_API_KEY)
@@ -70,6 +80,16 @@ fun HomeScreenContent(
     // To create an instance of the fused Location Provider Client
     val fusedLocationClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(context)
+
+    var latitude by remember {
+        mutableStateOf(0.0)
+    }
+    var longitude by remember {
+        mutableStateOf(0.0)
+    }
+    var showMap by remember {
+        mutableStateOf(false)
+    }
 
     val scope = rememberCoroutineScope()
     LaunchedEffect(key1 = Any()) {
@@ -86,14 +106,56 @@ fun HomeScreenContent(
                 rationaleMessage = "We apologize for the inconvenience, but unfortunately this " +
                         "permission is required to use the app",
                 shouldShowRationale = shouldShowRationale
-            ) {
-
-            }
+            ) { permissionState.launchPermissionRequest() }
         }
     ) {
+        //check to see if permission is available
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            try {
+                val locationResult = fusedLocationClient.getCurrentLocation(
+                    Priority.PRIORITY_HIGH_ACCURACY,
+                    CancellationTokenSource().token
+                )
+                locationResult.addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        latitude = location.latitude
+                        longitude = location.longitude
+                        showMap = true
+                    }
+                }
+
+            } catch (e: Exception) {
+                Toast.makeText(context, "Error Fetching Location", Toast.LENGTH_LONG).show()
+            }
+        }
+        if(showMap && !lo){
+
+            //showHomescreenContent
+
+            HomeScreenScaffold(state = )
+        }
 
     }
 
+
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
+@Composable
+fun HomeScreenScaffold(
+    state: HomeUiState,
+    onEvent: (HomeUiEvents) -> Unit,
+) {
 
     val restaurants = state.restaurants.collectAsLazyPagingItems()
 
@@ -161,7 +223,12 @@ fun HomeScreenContent(
 
         }
     }
+
 }
+
+
+
+
 
 
 fun Restaurant.toRestaurantItem(
